871a1cdda8c51abd86b3ba14aca868cab345d853
MatheusAraujo02 <127919173+MatheusAraujo02@users.noreply.github.com>
2024-06-11 20:16:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require("express");
 
 const app = express();
 
-// const porta = process.env.PORT || 3333;
-const porta = 3333;
+// const porta = process.env.PORT || 3306;
+const porta = 3306;
 
 app.listen(porta, () => {
    console.log(`Servidor iniciado na porta ${porta}`);
 });
+]
